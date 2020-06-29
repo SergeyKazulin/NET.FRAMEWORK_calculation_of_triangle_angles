6ee07debc333f4338ee1b5dacd33c4dc9b47ec07
Sergey Kazulin <SergeyKazulin@hotmail.com>
2020-06-29 15:33:07 +0300

    Добавьте файлы проекта.

---
+++ NET.FRAMEWORK Углы прямоугольного треугольника/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NET.FRAMEWORK_Углы_прямоугольного_треугольника
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double a, b, A, A1, B;
+            Console.Write("Введите сторону а: ");
+            a = Int32.Parse(Console.ReadLine());   // конвертированние Parse
+
+            Console.Write("Введите сторону b: ");
+            b = Convert.ToInt32(Console.ReadLine()); // конвертирование Convert
+
+            A1 = Math.Sin(a / b);
+            A = A1 * 180 / Math.PI;
+            B = 180 - 90 - A;
+
+            Console.WriteLine("Угол A = " + String.Format("{0:f2}", A));
+            Console.WriteLine("Угол В = " + String.Format("{0:f2}", B));
+
+
+        }
+    }
+}
